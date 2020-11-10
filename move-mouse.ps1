b87925f78ae56017a765bbf5e1a85d1c21f69412
Paulo Eduardo Althoff <pealthoff@gmail.com>
2020-11-09 21:53:07 -0300

    Create move-mouse.ps1

---
+++ move-mouse.ps1
@@ -0,0 +1,18 @@
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+ 
+Clear-Host
+Echo "moving test"
+Add-Type -AssemblyName System.Windows.Forms
+ 
+$signal = 1
+while($true)
+{
+    $p = [System.Windows.Forms.Cursor]::Position
+    Echo $p
+    $x = $p.X + $signal * 1000
+    $y = $p.Y + $signal * 1000
+    [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
+    Start-Sleep -Seconds 10 * 60
+    $signal *= -1
+}
